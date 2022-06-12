7d15621c6a6e5391834d37421c3887adb3d2aa5e
madalinamart <91364600+madalinamart@users.noreply.github.com>
2022-06-12 18:22:57 +0300

    changed db con for heroku deployment

--- db/conn.php
+++ db/conn.php
@@ -1,15 +1,15 @@
 <?php
-/*  $host = 'remotemysql.com';
+ $host = 'remotemysql.com';
  $db = 'psORmmetlI';
  $user = 'psORmmetlI';
  $pass = 'BlqmDLqObQ';
- $charset = 'utf8mb4'; */
+ $charset = 'utf8mb4';
 
- $host = '127.0.0.1';
+/*  $host = '127.0.0.1';
  $db = 'attendance_db';
  $user = 'root';
  $pass = '';
- $charset = 'utf8mb4';
+ $charset = 'utf8mb4'; */
 
 
  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
